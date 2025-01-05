fd2e638d9360fdaffb32b06ddcc7b99eeb3ad03b
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-05 14:42:41 +0900

    fix : 유저가 하나의 게시물에 여러번 좋아요 못누르게 방지

--- src/main/java/com/example/demo/src/like/entity/Like.java
+++ src/main/java/com/example/demo/src/like/entity/Like.java
@@ -13,7 +13,10 @@ import javax.persistence.*;
 @EqualsAndHashCode(callSuper = false)
 @Getter
 @Entity // 필수, Class 를 Database Table화 해주는 것이다
-@Table(name = "Like") // Table 이름을 명시해주지 않으면 class 이름을 Table 이름으로 대체한다.
+@Table(name = "Like",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "post_id"})
+}) // Table 이름을 명시해주지 않으면 class 이름을 Table 이름으로 대체한다.
 public class Like {
 
     @Id
