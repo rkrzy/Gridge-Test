05b185e47cb85d1f366db91f5e2ec84e932d3ac2
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-05 22:17:10 +0900

    fix : ProfileImage BaseEntity추가

--- src/main/java/com/example/demo/src/image/entity/ProfileImage.java
+++ src/main/java/com/example/demo/src/image/entity/ProfileImage.java
@@ -1,6 +1,7 @@
 package com.example.demo.src.image.entity;
 
 
+import com.example.demo.common.entity.BaseEntity;
 import com.example.demo.src.user.entity.User;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -14,7 +15,7 @@ import javax.persistence.*;
 @Getter
 @Entity // 필수, Class 를 Database Table화 해주는 것이다
 @Table(name = "PROFILEIMAGE") // Table 이름을 명시해주지 않으면 class 이름을 Table 이름으로 대체한다.
-public class ProfileImage {
+public class ProfileImage extends BaseEntity {
 
     @Id
     @Column(name = "id", nullable = false, updatable = false)
