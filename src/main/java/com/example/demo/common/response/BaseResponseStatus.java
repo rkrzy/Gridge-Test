4f464a35cc585aa1417db6d2d913f02ca73d3056
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-05 10:40:39 +0900

    feat : NOT_FIND_REPORT 일치하는 신고가 없음 상태 추가

--- src/main/java/com/example/demo/common/response/BaseResponseStatus.java
+++ src/main/java/com/example/demo/common/response/BaseResponseStatus.java
@@ -34,6 +34,7 @@ public enum BaseResponseStatus {
     INVALID_JWT(false, HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,HttpStatus.FORBIDDEN.value(),"권한이 없는 유저의 접근입니다."),
     NOT_FIND_USER(false,HttpStatus.NOT_FOUND.value(),"일치하는 유저가 없습니다."),
+    NOT_FIND_REPORT(false,HttpStatus.NOT_FOUND.value(),"일치하는 신고가 없습니다."),
     INVALID_OAUTH_TYPE(false, HttpStatus.BAD_REQUEST.value(), "알 수 없는 소셜 로그인 형식입니다."),
 
 
