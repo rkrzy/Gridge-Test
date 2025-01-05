fafc11d58feb2659c3ad8710e1b6eeac98422d1e
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-05 12:06:26 +0900

    feat : UserInfoDTO추가

--- src/main/java/com/example/demo/src/user/model/UserInfoDTO.java
+++ src/main/java/com/example/demo/src/user/model/UserInfoDTO.java
@@ -1,2 +1,17 @@
-package com.example.demo.src.user.model;public class UserInfoDTO {
+package com.example.demo.src.user.model;
+
+
+import com.example.demo.common.entity.BaseEntity.State;
+
+public class UserInfoDTO {
+    private Long id;
+    private String name;
+    private State state;
+
+    public UserInfoDTO(Long id, String name, State state)
+    {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
 }
