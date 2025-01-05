d4eb795a6f7ca181e2c93c24fc06aa1fdbd035eb
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-05 11:50:41 +0900

    feat : Image객체 추가

---
+++ src/main/java/com/example/demo/src/image/Image.java
@@ -0,0 +1,29 @@
+package com.example.demo.src.image;
+
+import com.example.demo.src.post.entity.Post;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = false)
+@Getter
+@Entity // 필수, Class 를 Database Table화 해주는 것이다
+@Table(name = "Image") // Table 이름을 명시해주지 않으면 class 이름을 Table 이름으로 대체한다.
+public class Image {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
